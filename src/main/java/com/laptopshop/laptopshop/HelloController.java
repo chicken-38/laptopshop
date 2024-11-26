d91a44fccc44342e7e4d9087dd9c85ae5240e0a9
A Chicken <hoangdc3896@gmail.com>
2024-11-27 00:10:48 +0700

    end chapter 7

--- src/main/java/com/laptopshop/laptopshop/HelloController.java
+++ src/main/java/com/laptopshop/laptopshop/HelloController.java
@@ -12,12 +12,12 @@ public class HelloController {
 
     @GetMapping("/user")
     public String userPage() {
-        return "Only user can access this page!";
+        return "Only user can access this page";
     }
 
     @GetMapping("/admin")
     public String adminPage() {
-        return "Only admin can access this page!";
+        return "Only admin can access this page";
     }
 
 }
